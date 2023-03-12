429a57af5786ddc70727babb78538ced7fcc677c
leehyunkyung <ten101ww@gmail.com>
2023-03-12 22:58:31 +0900

    COMEDY 장르 렌탈 추가/ 새로운 영수증 출력

--- Program.cs
+++ Program.cs
@@ -15,6 +15,9 @@ namespace VideoRental
             Movie newRelease2 = new Movie( "신작 2",Movie.NEW_RELEASE );
             Movie children1 = new Movie( "어린이 1", Movie.CHILDRENS );
             Movie children2 = new Movie( "어린이 2", Movie.CHILDRENS );
+            Movie comedy1 = new Movie("코미디 1", Movie.COMEDY);
+            Movie comedy2 = new Movie("코미디 2", Movie.COMEDY);
+
             Customer customer = new Customer("고객");
 
             customer.addRental(new Rental( regular1, 2 ));
@@ -23,8 +26,11 @@ namespace VideoRental
             customer.addRental(new Rental( newRelease2, 2 ));
             customer.addRental(new Rental( children1, 3 ));
             customer.addRental(new Rental( children2, 4 ));
+            customer.addRental(new Rental( comedy1, 1 ));
+            customer.addRental(new Rental( comedy2, 2 ));
 
-            Console.Write(customer.statement());
+            Console.WriteLine(customer.statement());
+            Console.WriteLine(customer.printReceipt());
         }
     }
 }
