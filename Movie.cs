4e55305260c99bd1fafcce268b72a0e0a7aedf78
leehyunkyung <ten101ww@gmail.com>
2023-03-12 22:46:25 +0900

    COMEDY 새장르 추가

--- Movie.cs
+++ Movie.cs
@@ -5,23 +5,24 @@ using System.Text;
 
 namespace VideoRental
 {
-  public class Movie
-  {
-    public const int CHILDRENS = 2;
-    public const int REGULAR = 0;
-    public const int NEW_RELEASE = 1;
-
-    public Movie(string title, int priceCode = REGULAR)
+    public class Movie
     {
-        movieTitle = title;
-        moviePriceCode = priceCode;
+        public const int CHILDRENS = 2;
+        public const int REGULAR = 0;
+        public const int NEW_RELEASE = 1;
+        public const int COMEDY = 3;
+
+        public Movie(string title, int priceCode = REGULAR)
+        {
+            movieTitle = title;
+            moviePriceCode = priceCode;
+        }
+
+        public int getPriceCode() { return moviePriceCode; }
+        public void setPriceCode(int args) { moviePriceCode = args; }
+        public string getTitle() { return movieTitle; }
+
+        private string movieTitle;
+        int moviePriceCode;
     }
-
-    public int getPriceCode() { return moviePriceCode; }
-    public void setPriceCode(int args) { moviePriceCode = args; }
-    public string getTitle() { return movieTitle; }
-
-    private string movieTitle;
-    int moviePriceCode;
-  }
 }
